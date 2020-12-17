c5df4477f1f090cb13847698d2695c5d3cb96f41
talis-fb <talisonf11@gmail.com>
2020-12-16 21:36:44 -0300

    Estilo dos blocos de Pergunta da página de criar salas

--- src/pages/CreateRoom/components/question.js
+++ src/pages/CreateRoom/components/question.js
@@ -5,8 +5,35 @@ import './Question.scss'
 class Question extends React.Component {
     render(){
         return(
-            <div className="question">
-
+            <div className="block-question">
+                <section className="number">
+                    <h2>{this.props.number}</h2> 
+                    <button className="remove-question">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+                        </svg>
+                    </button>
+                </section>
+                <section className="main">
+                    <div className="question">
+                        <textarea placeholder="Question..."></textarea>
+                    </div>
+                    <div className="answers">
+                        <div className="triangle">
+                            <input type="text">
+                            </input>
+                        </div>
+                        <div className="lozenge">
+                            <input type="text"></input>
+                        </div>
+                        <div className="circle">
+                            <input type="text"></input>
+                        </div>
+                        <div className="square">
+                            <input type="text"></input>
+                        </div>
+                    </div>
+                </section>
             </div>
         )
     }
--- src/pages/CreateRoom/index.js
+++ src/pages/CreateRoom/index.js
@@ -5,29 +5,47 @@ import Question from './components/Question'
 
 import './index.scss'
 
-function createRoom(){
-    return (
-        <div className="createRoom">
-            <header className="header-createRoom">
-                <div className="logo">
-                    <a href="/" className="homeLink">Yakoot!</a>
-                </div>
+class createRoom extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            questions: [1]
+        }
+    }
+    render(){
+        return (
+            <div className="createRoom">
+                <header className="header-createRoom">
+                    <div className="logo">
+                        <a href="/" className="homeLink">Yakoot!</a>
+                    </div>
 
-                <BlockAboutQuiz />
+                    <BlockAboutQuiz />
 
-                <div className="finishQuiz">
-                    <button type="button" class="btn btn-success">Finish</button>
-                </div>
-            </header>
+                    <div className="finishQuiz">
+                        <button type="button" class="btn btn-success">Finish</button>
+                    </div>
+                </header>
 
-            <main className="question-choosed">
-               <button id="addQuestion" className="btn btn-success">+ ADD QUESTION</button>
-               <Question />
-            </main>
+                <main className="question-choosed">
+                    <button 
+                        id="addQuestion" 
+                        className="btn btn-success" 
+                        onClick={ () => {
+                            this.setState({ questions: [...this.state.questions, this.state.questions.length+1 ] })
+                            console.log(this.state)
+                            }
+                        }
+                    >
+                        + ADD QUESTION
+                    </button>
 
-            
-        </div>
-    )
+                    {/* Block of Questions */}
+                    {this.state.questions.map( print => <Question  number={print} /> )} 
+                </main>  
+            </div>
+        )
+    }
 }
 
 export default createRoom
